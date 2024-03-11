2fd2858faa4c7f1a0d5285def2a999b3d4abf59c
gwenvalia <baradeau.lea@gmail.com>
2024-03-11 15:31:08 +0100

    add bo and add bdd

---
+++ src/Entity/Sortie.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SortieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SortieRepository::class)]
+class Sortie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateTimeStart = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $duration = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateInscriptionLimit = null;
+
+    #[ORM\Column]
+    private ?int $maxInscriptionNb = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\ManyToOne(inversedBy: 'sorties')]
+    private ?Etat $etat = null;
+
+    #[ORM\ManyToOne(inversedBy: 'sorties')]
+    private ?Lieu $lieu = null;
+
+    #[ORM\ManyToOne(inversedBy: 'sorties')]
+    private ?Site $siteOrga = null;
+
+    #[ORM\ManyToMany(targetEntity: Participant::class, mappedBy: 'sortiesInscri')]
+    private Collection $participants;
+
+    #[ORM\ManyToOne(inversedBy: 'sortieOrga')]
+    private ?Participant $organisateur = null;
+
+    public function __construct()
+    {
+        $this->participants = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDateTimeStart(): ?\DateTimeInterface
+    {
+        return $this->dateTimeStart;
+    }
+
+    public function setDateTimeStart(\DateTimeInterface $dateTimeStart): static
+    {
+        $this->dateTimeStart = $dateTimeStart;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(?int $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getDateInscriptionLimit(): ?\DateTimeInterface
+    {
+        return $this->dateInscriptionLimit;
+    }
+
+    public function setDateInscriptionLimit(\DateTimeInterface $dateInscriptionLimit): static
+    {
+        $this->dateInscriptionLimit = $dateInscriptionLimit;
+
+        return $this;
+    }
+
+    public function getMaxInscriptionNb(): ?int
+    {
+        return $this->maxInscriptionNb;
+    }
+
+    public function setMaxInscriptionNb(int $maxInscriptionNb): static
+    {
+        $this->maxInscriptionNb = $maxInscriptionNb;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getEtat(): ?Etat
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(?Etat $etat): static
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getLieu(): ?Lieu
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(?Lieu $lieu): static
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getSiteOrga(): ?Site
+    {
+        return $this->siteOrga;
+    }
+
+    public function setSiteOrga(?Site $siteOrga): static
+    {
+        $this->siteOrga = $siteOrga;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Participant>
+     */
+    public function getParticipants(): Collection
+    {
+        return $this->participants;
+    }
+
+    public function addParticipant(Participant $participant): static
+    {
+        if (!$this->participants->contains($participant)) {
+            $this->participants->add($participant);
+            $participant->addSortiesInscri($this);
+        }
+
+        return $this;
+    }
+
+    public function removeParticipant(Participant $participant): static
+    {
+        if ($this->participants->removeElement($participant)) {
+            $participant->removeSortiesInscri($this);
+        }
+
+        return $this;
+    }
+
+    public function getOrganisateur(): ?Participant
+    {
+        return $this->organisateur;
+    }
+
+    public function setOrganisateur(?Participant $organisateur): static
+    {
+        $this->organisateur = $organisateur;
+
+        return $this;
+    }
+}
